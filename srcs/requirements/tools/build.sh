1f816094e14788d8b81a9f9b150828ebbe16a029
thibaut1304 <thibaut.husser@free.fr>
2022-06-07 00:22:39 +0200

    fix buf make after down and fix tag latest

--- srcs/requirements/tools/build.sh
+++ srcs/requirements/tools/build.sh
@@ -6,8 +6,15 @@ if [[ $(cat /etc/hosts | grep "${DOMAIN_NAME}" | wc -l) -eq 0 ]] ; then
 	# echo "127.0.0.1 ${DOMAIN_NAME}" >> /etc/hosts
 	echo "127.0.0.1 ${DOMAIN_NAME}" | sudo tee -a /etc/hosts > /dev/null
 fi
-sudo mkdir -p -v ${PATH_HOST}
-sudo mkdir -p -v ${PATH_HOST}/db
-sudo mkdir -p -v ${PATH_HOST}/wordpress
-sudo chown -R $USER:$USER ${PATH_HOST}
-sudo chmod -R 755 ${PATH_HOST}
+
+if [[ ! -d ${PATH_HOST} ]]; then
+	sudo mkdir -p -v ${PATH_HOST}
+	sudo chown -R $USER:$USER ${PATH_HOST}
+	sudo chmod -R 755 ${PATH_HOST}
+fi
+if [[ ! -d ${PATH_HOST}/db ]]; then
+	sudo mkdir -p -v ${PATH_HOST}/db
+fi
+if [[ ! -d ${PATH_HOST}/wordpress ]]; then
+	sudo mkdir -p -v ${PATH_HOST}/wordpress
+fi
